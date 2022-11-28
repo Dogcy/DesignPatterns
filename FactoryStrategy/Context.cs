57f8b4bf78f6ffba94605a0e5ad87ce9d279256e
ec-CY <>
2022-11-28 15:22:41 +0800

    FactoryStrategy 工廠+策略
    可觀察與OperationFactory差異

---
+++ FactoryStrategy/Context.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryStrategy
+{
+
+    internal abstract class Strategy
+    {
+        public abstract void AlorithmInterFace();
+    }
+    internal class ConcreteStategyA : Strategy
+    {
+        public override void AlorithmInterFace()
+        {
+            Console.WriteLine("演算法A實現");
+        }
+    }
+    class ConcreteStategyB : Strategy
+    {
+        public override void AlorithmInterFace()
+        {
+            Console.WriteLine("演算法B實現");
+        }
+    }
+    class ConcreteStategyC : Strategy
+    {
+        public override void AlorithmInterFace()
+        {
+            Console.WriteLine("演算法c實現");
+        }
+    }
+    /// <summary>
+    /// 此處為策略模式
+    /// </summary>
+    internal class Context
+    {
+        Strategy strategy;
+        public Context(Input input)
+        {
+            this.strategy = input switch
+            {
+                Input.A => new ConcreteStategyA(),
+                Input.B => new ConcreteStategyB(),
+                Input.C => new ConcreteStategyC()
+
+            };
+        }
+
+        public void ContextInterface()
+        {
+            this.strategy.AlorithmInterFace();
+        }
+    }
+    enum Input
+    {
+        A,
+        B,
+        C        
+    }
+}
---
+++ FactoryStrategy/Program.cs
@@ -0,0 +1,8 @@
+﻿using FactoryStrategy;
+
+/*  
+    可觀察與OperationFactory差異
+    在於減少一個類別的使用(static factory)    
+ */
+Context context = new Context(Input.A);
+context.ContextInterface();
